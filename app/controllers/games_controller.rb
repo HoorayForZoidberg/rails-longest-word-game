04f33528732c1d00b103c4217e94f734ff0b8773
Nicolas Lizop <nicolas.lizop@gmail.com>
2017-11-20 21:23:04 +0100

    seems to work, but some odd syntax highlight

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -10,30 +10,32 @@ class GamesController < ApplicationController
     @start_time = Time.now
   end
 
-  def score(word)
-    end_time = Time.now
-    results = run_game(word, @letter, @start_time, end_time)
+  def score
+    @word = params[:word]
+    @grid = params[:grid].split(" ")
+    @start_time = params[:start_time].to_i
+    @end_time = Time.now.to_i
+    @results = run_game(@word, @grid, @start_time, @end_time)
   end
 
   private
 
   def run_game(attempt, grid, start_time, end_time)
     # TODO: runs the game and return detailed hash of resultrake
-    result_hash = { time: end_time - start_time, score: 0.0, message: "Well done!" }
+    @result_hash = { time: end_time - start_time, score: 0.0, message: "Well done!" }
 
     # retrieve JSON object
     api_hash = JSON.parse(open("https://wagon-dictionary.herokuapp.com/#{attempt}").read)
 
     # check if word is english, and in the grid
     if api_hash["found"] == false
-      result_hash[:message] = "Word not found. Probably not an English word."
+      @result_hash[:message] = "Word not found. Probably not an English word."
     elsif !check_in_grid?(attempt, grid)
-      result_hash[:message] = "The given word is not in the grid"
+      @result_hash[:message] = "The given word is not in the grid"
     else
-      result_hash[:score] = api_hash["length"].to_f + (8.0 / result_hash[:time])
+      @result_hash[:score] = api_hash["length"].to_f + (8.0 / @result_hash[:time]) # no idea what this problem is...
+      @result_hash[:message] = "Well done!"
     end
-
-    return result_hash
   end
 
   def hashed(string)
